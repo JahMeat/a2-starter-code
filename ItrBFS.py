da4e80d868cc650daba6d1e39518793de0665e12
eugene-cheung <eugenus1@uw.edu>
2025-01-21 23:02:59 -0800

    problem 7 finished

--- ItrBFS.py
+++ ItrBFS.py
@@ -33,8 +33,54 @@ class ItrBFS:
         print("\nWelcome to ItrBFS")
 
     def runBFS(self):
-        # Comment out the line below when this function is implemented.
-        raise NotImplementedError
+        initial_state = self.Problem.CREATE_INITIAL_STATE()
+        self.COUNT = 0
+        self.MAX_OPEN_LENGTH = 0
+        self.BACKLINKS = {initial_state: None}
+        open_list = [initial_state]
+        closed_list = []
+
+        while open_list:
+            self.MAX_OPEN_LENGTH = max(self.MAX_OPEN_LENGTH, len(open_list))
+
+            current_state = open_list.pop(0)
+            closed_list.append(current_state)
+
+            if self.Problem.GOAL_TEST(current_state):
+                print(f"Goal reached: {current_state}")
+                self.PATH = self.backtrace(current_state)
+                self.PATH_LENGTH = len(self.PATH) - 1
+                print(f"Solution path: {self.PATH}")
+                print(f"Path length: {self.PATH_LENGTH}")
+                print(f"Nodes expanded: {self.COUNT}")
+                print(f"Max open list length: {self.MAX_OPEN_LENGTH}")
+                return
+
+            self.COUNT += 1
+            successors = self.Problem.OPERATORS
+
+            for operator in successors:
+                if operator.precond(current_state):
+                    successor_state = operator.state_transf(current_state)
+
+                    if successor_state not in closed_list and successor_state not in open_list:
+                        open_list.append(successor_state)
+                        self.BACKLINKS[successor_state] = current_state
+
+        print("No solution found.")
+        return
+
+    def backtrace(self, state):
+        """
+        Backtrack from the goal state to the initial state using backlinks.
+        """
+        path = []
+        while state is not None:
+            path.append(state)
+            state = self.BACKLINKS[state]
+        path.reverse()  # Reverse the path to start from the initial state.
+        return path
+
 
 
 if __name__ == '__main__':
